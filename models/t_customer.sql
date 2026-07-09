c7a820d87c3c4bacf9f399afe2abc3e59a8754b3
Prudhvi Raj M <prudhvirajm25@gmail.com>
2026-07-09 19:36:25 +0000

    commit

--- models/t_customer.sql
+++ models/t_customer.sql
@@ -1,7 +1,1 @@
-{{
-    config(
-        materialized='table',
-        transient=false
-    )
-}}
-select customer_id,concat(first_name,'_',last_name) as full_name from dev_db.bronze.customers
+select customer_id,concat(first_name,'_',last_name) as full_name from {{ source('s1', 'customers') }}
